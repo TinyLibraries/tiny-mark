c43d606856d0115fd45a2f4a2c44645bfd333f2c
John Kahn <john.kahn@aa.com>
2019-12-23 16:21:00 -0600

    Add comments to browserExport.js

--- src/browserExport.js
+++ src/browserExport.js
@@ -1,1 +1,4 @@
+// This file is used during the build step to output a global function for the browser //
+//                                    DO NOT MODIFY                                    //
+
 module.exports = require("./index").default;
